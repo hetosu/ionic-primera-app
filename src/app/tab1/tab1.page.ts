8575f08d9781c6a593e888dc725039ef29b32567
htobio <htobio@atsistemas.com>
2020-09-15 11:16:46 +0200

    Fin sección 2

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,12 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })
-export class Tab1Page {
+export class Tab1Page implements OnInit{
 
-  constructor() {}
+  constructor() {
+
+    let num = 10;
+    
+    num = 20;
+
+    num = ++num;
+
+    num = 0;
+
+    console.log('Hola mundo');
+
+  }
 
 }
